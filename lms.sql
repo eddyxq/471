db247d9d357138718080c5a48daf0a31da254c2b
Trilok Patel <33501271+patelTrilok@users.noreply.github.com>
2019-04-09 22:36:02 -0600

    The SQL file for the database
    SQL file for the project. To load it, first create a database called "lms"

---
+++ lms.sql
@@ -0,0 +1,505 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 10, 2019 at 06:34 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `UserID` int(11) NOT NULL,
+  `EmployeeID` int(11) NOT NULL,
+  `Date_Hired` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author`
+--
+
+CREATE TABLE `author` (
+  `First_Name` varchar(50) NOT NULL,
+  `Middle_Name` varchar(15) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Date_Born` date NOT NULL,
+  `Date_Died` date DEFAULT NULL,
+  `Origin_Country` varchar(50) NOT NULL,
+  `Writing_Style` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `available`
+--
+
+CREATE TABLE `available` (
+  `Room_Number` int(11) NOT NULL,
+  `Date_Available` date NOT NULL,
+  `Time_Available` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `Item_ID` int(11) NOT NULL,
+  `ISBN` int(13) NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `Edition` tinyint(4) NOT NULL,
+  `Language` varchar(25) NOT NULL,
+  `Publisher_Name` varchar(50) NOT NULL,
+  `Publisher_ID` int(11) NOT NULL,
+  `Publish_Date` date NOT NULL,
+  `Lendable` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booked`
+--
+
+CREATE TABLE `booked` (
+  `Room_Number` int(11) NOT NULL,
+  `Date_Booked` date NOT NULL,
+  `Time_Booked` time NOT NULL,
+  `Booking_Duration` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrows`
+--
+
+CREATE TABLE `borrows` (
+  `UserID` int(11) NOT NULL,
+  `ISBN` int(13) NOT NULL,
+  `Date_Borrowed` date NOT NULL,
+  `Date_Returned` date DEFAULT NULL,
+  `Borrow_Duration` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fees`
+--
+
+CREATE TABLE `fees` (
+  `UserID` int(11) NOT NULL,
+  `Type` varchar(50) NOT NULL,
+  `Amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genre`
+--
+
+CREATE TABLE `genre` (
+  `ISBN` int(13) NOT NULL,
+  `Genre` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `Item_ID` int(11) NOT NULL,
+  `Item_Type` varchar(50) NOT NULL,
+  `Item_Location` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lends`
+--
+
+CREATE TABLE `lends` (
+  `UserID` int(11) NOT NULL,
+  `EmployeeID` int(11) NOT NULL,
+  `ISBN` int(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `librarian`
+--
+
+CREATE TABLE `librarian` (
+  `UserID` int(11) NOT NULL,
+  `EmployeeID` int(11) NOT NULL,
+  `Date_Hired` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `library`
+--
+
+CREATE TABLE `library` (
+  `Name` varchar(50) NOT NULL,
+  `Location` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `UserID` int(11) NOT NULL,
+  `Num_Of_Books_Borrowed` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `Name` varchar(50) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `Location` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserves`
+--
+
+CREATE TABLE `reserves` (
+  `UserID` int(11) NOT NULL,
+  `Room_Number` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `study_rooms`
+--
+
+CREATE TABLE `study_rooms` (
+  `Room_Number` int(11) NOT NULL,
+  `Capacity` int(11) NOT NULL,
+  `Lib_Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `UserID` int(11) NOT NULL,
+  `First_Name` varchar(50) NOT NULL,
+  `Middle_Name` varchar(15) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Street_Name` varchar(50) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `Phone_Number` int(10) NOT NULL,
+  `Lib_Name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writes`
+--
+
+CREATE TABLE `writes` (
+  `Author_Fname` varchar(50) NOT NULL,
+  `Author_Mname` varchar(15) NOT NULL,
+  `Author_Lname` varchar(50) NOT NULL,
+  `ISBN` int(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`UserID`,`EmployeeID`) USING BTREE,
+  ADD KEY `EmployeeID` (`EmployeeID`);
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`First_Name`,`Middle_Name`,`Last_Name`);
+
+--
+-- Indexes for table `available`
+--
+ALTER TABLE `available`
+  ADD PRIMARY KEY (`Room_Number`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`ISBN`),
+  ADD KEY `Item_ID` (`Item_ID`),
+  ADD KEY `Publisher_Name` (`Publisher_Name`);
+
+--
+-- Indexes for table `booked`
+--
+ALTER TABLE `booked`
+  ADD PRIMARY KEY (`Room_Number`);
+
+--
+-- Indexes for table `borrows`
+--
+ALTER TABLE `borrows`
+  ADD PRIMARY KEY (`UserID`,`ISBN`),
+  ADD KEY `ISBN` (`ISBN`);
+
+--
+-- Indexes for table `fees`
+--
+ALTER TABLE `fees`
+  ADD PRIMARY KEY (`UserID`,`Type`);
+
+--
+-- Indexes for table `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`ISBN`,`Genre`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`Item_ID`);
+
+--
+-- Indexes for table `lends`
+--
+ALTER TABLE `lends`
+  ADD PRIMARY KEY (`UserID`,`EmployeeID`,`ISBN`),
+  ADD KEY `EmployeeID` (`EmployeeID`),
+  ADD KEY `ISBN` (`ISBN`);
+
+--
+-- Indexes for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD PRIMARY KEY (`UserID`,`EmployeeID`),
+  ADD KEY `EmployeeID` (`EmployeeID`);
+
+--
+-- Indexes for table `library`
+--
+ALTER TABLE `library`
+  ADD PRIMARY KEY (`Name`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`UserID`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`Name`,`ID`);
+
+--
+-- Indexes for table `reserves`
+--
+ALTER TABLE `reserves`
+  ADD PRIMARY KEY (`UserID`,`Room_Number`),
+  ADD KEY `Room_Number` (`Room_Number`);
+
+--
+-- Indexes for table `study_rooms`
+--
+ALTER TABLE `study_rooms`
+  ADD PRIMARY KEY (`Room_Number`),
+  ADD KEY `Lib_Name` (`Lib_Name`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`UserID`),
+  ADD KEY `user_ibfk_1` (`Lib_Name`);
+
+--
+-- Indexes for table `writes`
+--
+ALTER TABLE `writes`
+  ADD PRIMARY KEY (`Author_Fname`,`Author_Mname`,`Author_Lname`,`ISBN`),
+  ADD KEY `ISBN` (`ISBN`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `Item_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `librarian`
+--
+ALTER TABLE `librarian`
+  MODIFY `EmployeeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD CONSTRAINT `administrator_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `available`
+--
+ALTER TABLE `available`
+  ADD CONSTRAINT `available_ibfk_1` FOREIGN KEY (`Room_Number`) REFERENCES `study_rooms` (`Room_Number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`Item_ID`) REFERENCES `inventory` (`Item_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_ibfk_2` FOREIGN KEY (`Publisher_Name`) REFERENCES `publisher` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `booked`
+--
+ALTER TABLE `booked`
+  ADD CONSTRAINT `booked_ibfk_1` FOREIGN KEY (`Room_Number`) REFERENCES `study_rooms` (`Room_Number`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `borrows`
+--
+ALTER TABLE `borrows`
+  ADD CONSTRAINT `borrows_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `member` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `borrows_ibfk_2` FOREIGN KEY (`ISBN`) REFERENCES `member` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `fees`
+--
+ALTER TABLE `fees`
+  ADD CONSTRAINT `fees_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `genre`
+--
+ALTER TABLE `genre`
+  ADD CONSTRAINT `genre_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `lends`
+--
+ALTER TABLE `lends`
+  ADD CONSTRAINT `lends_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `librarian` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `lends_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `librarian` (`EmployeeID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `lends_ibfk_3` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `librarian`
+--
+ALTER TABLE `librarian`
+  ADD CONSTRAINT `librarian_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
+
+--
+-- Constraints for table `member`
+--
+ALTER TABLE `member`
+  ADD CONSTRAINT `member_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `reserves`
+--
+ALTER TABLE `reserves`
+  ADD CONSTRAINT `reserves_ibfk_1` FOREIGN KEY (`Room_Number`) REFERENCES `study_rooms` (`Room_Number`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reserves_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `study_rooms`
+--
+ALTER TABLE `study_rooms`
+  ADD CONSTRAINT `study_rooms_ibfk_1` FOREIGN KEY (`Lib_Name`) REFERENCES `library` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`Lib_Name`) REFERENCES `library` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `writes`
+--
+ALTER TABLE `writes`
+  ADD CONSTRAINT `writes_ibfk_1` FOREIGN KEY (`ISBN`) REFERENCES `book` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `writes_ibfk_2` FOREIGN KEY (`Author_Fname`) REFERENCES `author` (`First_Name`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
