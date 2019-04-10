edc206424b71bc69eea3ba6ae77daae4cbb67ee7
eddy <eddy@ualberta.ca>
2019-04-10 16:45:03 -0600

    implemented add, update, delete

--- webserver/view.php
+++ webserver/view.php
@@ -1,28 +1,30 @@
-<?php
+
+<html>
+	<body>
+			<?php
 
 // Create connection
 $con=mysqli_connect("localhost","root","","471");
 
 // Check connection
-if (mysqli_connect_errno($con))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+if (mysqli_connect_errno($con)){
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
  
-if ($_GET["job"] == "update"){
-
-$ID = $_POST["ID"];
-$name = $_POST["name"];
-$email = $_POST["email"];
- 
-$result = mysqli_query($con,"update Users set name='".$name. "', email='". $email. "' where ID=". $ID);
-
+if(isset($_GET['job'])){
+	if (($_GET['job']) == "update"){
+		$ID = $_POST["ID"];
+		$name = $_POST["name"];
+		$email = $_POST["email"];
+		$result = mysqli_query($con,"update Users set name='".$name. "', email='". $email. "' where ID=". $ID);
+	}
 } 
-  
-if ($_GET["job"] == "delete"){
-$ID = $_GET["ID"];
-$result = mysqli_query($con,"Delete from Users where ID=". $ID );
+if(isset($_GET['job'])){
+	if ($_GET['job'] == "delete"){
+	$ID = $_GET["ID"];
+	$result = mysqli_query($con,"Delete from Users where ID=". $ID );
 
+	}
 }
 
 $result = mysqli_query($con,"SELECT * FROM Users");
@@ -34,21 +36,27 @@ echo "<table border='1'>
 <th>Email</th>
 </tr>";
 
-while($row = mysqli_fetch_array($result))
-  {
-  echo "<tr>";
-  echo "<td>" . $row['ID'] . "</td>";
-  echo "<td>" . $row['Name'] . "</td>";
-  echo "<td>" . $row['Email'] . "</td>";
-  echo "<td><a href='update.php?ID= " . $row['ID'] . "'>Update</a></td>";
-  echo "<td><a onClick= \"return confirm('Do you want to delete this user?')\" href='view.php?job=delete&amp;ID= " . $row['ID'] . "'>DELETE</a></td>";
-  
-  echo "</tr>";
-  }
+while($row = mysqli_fetch_array($result)){
+	echo "<tr>";
+	echo "<td>" . $row['ID'] . "</td>";
+	echo "<td>" . $row['NAME'] . "</td>";
+	echo "<td>" . $row['Email'] . "</td>";
+	echo "<td><a href='update.php?ID= " . $row['ID'] . "'>Update</a></td>";
+	echo "<td><a onClick= \"return confirm('Do you want to delete this user?')\" href='view.php?job=delete&amp;ID= " . $row['ID'] . "'>DELETE</a></td>";
+
+	echo "</tr>";
+}
+
 echo "</table>";
-
-
-
 
 mysqli_close($con);
 ?>
+
+			<form action="admin.php" method="post">
+				<input type="submit" value="back">
+			</form>
+		
+	</body>
+</html>
+
+
