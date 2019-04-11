d3327ad3ed376e2d938440c3dde8f0871a7369c6
eddyxq <eddy@ualberta.ca>
2019-04-10 23:38:52 -0600

    full admin gui

--- webserver/addingUser.php
+++ webserver/addingUser.php
@@ -1,38 +1,41 @@
+<html>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<body>
+		<?php
+		$first_name = $_POST["first_name"];
+		$middle_name = $_POST["middle_name"];
+		$last_name = $_POST["last_name"];
+		$street_name = $_POST["street_name"];
+		$city = $_POST["city"];
+		$country = $_POST["country"];
+		$phone_number = $_POST["phone_number"];
 
-<?php
+		echo $first_name. "<br>".
+		$middle_name. "<br>".
+		$last_name. "<br>".
+		$street_name. "<br>".
+		$city. "<br>".
+		$country. "<br>".
+		$phone_number. "<br>";
 
-$first_name = $_POST["first_name"];
-$middle_name = $_POST["middle_name"];
-$last_name = $_POST["last_name"];
-$street_name = $_POST["street_name"];
-$city = $_POST["city"];
-$country = $_POST["country"];
-$phone_number = $_POST["phone_number"];
+		// Create connection
+		$con=mysqli_connect("localhost","root","","lms");
 
-echo $first_name. "<br>".
-$middle_name. "<br>".
-$last_name. "<br>".
-$street_name. "<br>".
-$city. "<br>".
-$country. "<br>".
-$phone_number. "<br>";
+		// Check connection
+		if (mysqli_connect_errno($con)){
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		}
+		  
+		$sql = "INSERT INTO user (first_name, middle_name, last_name, street_name, city, country, phone_number) VALUES ('". $first_name."','". $middle_name ."','". $last_name ."','". $street_name ."','". $city ."','". $country ."','". $phone_number ."')";
+		 
+		if (!mysqli_query($con,$sql)){
+			die('Error: ' . mysqli_error($con));
+		}
 
-// Create connection
-$con=mysqli_connect("localhost","root","","lms");
-
-// Check connection
-if (mysqli_connect_errno($con)){
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-  
-  $sql = "INSERT INTO user (first_name, middle_name, last_name, street_name, city, country, phone_number) VALUES ('". $first_name."','". $middle_name ."','". $last_name ."','". $street_name ."','". $city ."','". $country ."','". $phone_number ."')";
- 
-if (!mysqli_query($con,$sql)){
-	die('Error: ' . mysqli_error($con));
-}
-  
-echo "1 record added";
-
-mysqli_close($con);
-?>
-
+		mysqli_close($con);
+		?>
+	
+		<h1><p><font size="7" color="#ffffff">New User Added</font></p></h1>
+		<a class="button" href="admin.php">Back</a>
+	</body>
+</html>
--- webserver/adminAddUser.php
+++ webserver/adminAddUser.php
@@ -1,7 +1,61 @@
 <html>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<style>
+	input[type=text], select {
+	  width: 100%;
+	  padding: 12px 20px;
+	  margin: 8px 0;
+	  display: inline-block;
+	  border: 1px solid #ccc;
+	  border-radius: 4px;
+	  box-sizing: border-box;
+	}
+
+	input[type=submit] {
+	 width: 500px;
+		display: inline-block;
+		text-align: center;
+		vertical-align: middle;
+		padding: 40px 80px;
+		border: 1px solid #225594;
+		border-radius: 21px;
+		background: #40a0ff;
+		background: -webkit-gradient(linear, left top, left bottom, from(#40a0ff), to(#225594));
+		background: -moz-linear-gradient(top, #40a0ff, #225594);
+		background: linear-gradient(to bottom, #40a0ff, #225594);
+		-webkit-box-shadow: #4dc0ff 0px 0px 40px 0px;
+		-moz-box-shadow: #4dc0ff 0px 0px 40px 0px;
+		box-shadow: #4dc0ff 0px 0px 40px 0px;
+		text-shadow: #143257 1px 1px 1px;
+		font: normal normal bold 20px arial;
+		color: #ffffff;
+		text-decoration: none;
+	}
+
+	input[type=submit]:hover,
+	input[type=submit]:focus {
+		border: 1px solid ##a48d88;
+		background: #ffdcd4;
+		background: -webkit-gradient(linear, left top, left bottom, from(#ffdcd4), to(#9d8883));
+		background: -moz-linear-gradient(top, #ffdcd4, #9d8883);
+		background: linear-gradient(to bottom, #ffdcd4, #9d8883);
+		color: #ffffff;
+		text-decoration: none;
+	}
+	input[type=submit]:active {
+		background: #225594;
+		background: -webkit-gradient(linear, left top, left bottom, from(#225594), to(#225594));
+		background: -moz-linear-gradient(top, #225594, #225594);
+		background: linear-gradient(to bottom, #225594, #225594);
+	}
+
+	div {
+	  border-radius: 50px;
+	  padding: 50px 1000px 300px 50px;
+	}
+	</style>
 	<body>
-		<center>
+		<div>
 			<form action="addingUser.php" method="post">
 			   <font size="4" color="#ffffff">First Name</font><input type="text" name="first_name"><br>
 			   <font size="4" color="#ffffff">Middle Name</font><input type="text" name="middle_name"><br>
@@ -10,12 +64,13 @@
 			   <font size="4" color="#ffffff">City</font><input type="text" name="city"><br>
 			   <font size="4" color="#ffffff">Country</font><input type="text" name="country"><br>
 			   <font size="4" color="#ffffff">Phone Number</font><input type="text" name="phone_number"><br>
-			   <input type="submit" value="add">
+			   <input type="submit" value="Add">
 			</form>
 			
-			<form action="admin.php" method="post">
-				<input type="submit" value="back">
+			<form action="admin.php">
+				<input type="submit" value="Return">
 			</form>
-		</center>
+			
+		</div>
 	</body>
 </html>
